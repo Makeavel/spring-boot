89369fef43ea58a74210ed6de989fdf523e0f2db
Makeavel <dpseutroco@gmail.com>
2021-08-17 08:55:47 -0300

    spring3

--- spring3/src/main/java/com/phga/spring3/controller/empregoC.java
+++ spring3/src/main/java/com/phga/spring3/controller/empregoC.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class empregoC {
 
     @Autowired
