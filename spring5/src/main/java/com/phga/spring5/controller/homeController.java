5875b039b3e6c26c92ff2562967dfb41a25cd09e
Makeavel <dpseutroco@gmail.com>
2021-08-24 11:56:41 -0300

    Funções e Permissões

--- spring5/src/main/java/com/phga/spring5/controller/homeController.java
+++ spring5/src/main/java/com/phga/spring5/controller/homeController.java
@@ -1,9 +1,10 @@
 package com.phga.spring5.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class homeController {
     
     @GetMapping("/login")
@@ -16,5 +17,12 @@ public class homeController {
         return "index";
     }
 
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+
+    
 }
 
--- spring5/src/main/java/com/phga/spring5/securityConfiguration.java
+++ spring5/src/main/java/com/phga/spring5/securityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -23,21 +24,22 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-        .anyRequest()
-        .authenticated()
+        .antMatchers("/").access("hasAnyAuthority('USERS', 'ADMIN')")
+        .antMatchers("/admin").access("hasAnyAuthority('ADMIN')")
+        .anyRequest().authenticated()
         .and()
-        .formLogin()
-        .loginPage("/login")
-        .permitAll();
+        .formLogin().loginPage("/login").permitAll()
+        .and()
+        .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/login").permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
-        .withUser("user")
-        .password(passwordEncoder()
-        .encode("password"))
-        .authorities("USER");
+        .withUser("ph").password(passwordEncoder().encode("ffff")).authorities("ADMIN")
+        .and()
+        .withUser("user").password(passwordEncoder().encode("password")).authorities("USER");
 
     }
 }
